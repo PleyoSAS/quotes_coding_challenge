540c1c35f38791724e0dc3104983136e7a73a999
tohami <tohami1993@gmail.com>
2025-04-27 01:25:22 +0300

    create runApp function

--- bin/quotes_coding_challenge.dart
+++ bin/quotes_coding_challenge.dart
@@ -1,9 +1,13 @@
 import 'package:quotes_coding_challenge/quotes_service.dart';
 
-void main() async {
+Future<void> runApp() async {
   final quotesService = QuotesService();              // <-  hard dependency
   final quotes = await quotesService.fetchQuotes();
   for (final q in quotes) {
     print(q);
   }
+}
+
+void main() async {
+  await runApp();
 }
